44e1e37797abfa1bc39fadd11d0cfb2e8972f49a
Vedmid20 <lysyy.boba.freelance@gmail.com>
2025-02-07 14:40:09 +0200

    Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        light: "#d1a89e"
+      },
+
+    },
+  },
+  plugins: [],
+}
+
